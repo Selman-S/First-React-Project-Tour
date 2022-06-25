f08d260453df23787c4e30e66b67b257039556f4
selman <gunduzgece546@gmail.com>
2022-06-25 12:24:16 +0300

    nav created

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import './Main.scss';
+import {data} from '../../helpers/data';
+
+
+
+const Main =()=> {
+
+  console.log(data)
+  return (
+    <div className="card-container">
+      <h1>Card</h1>
+    </div>
+  )
+}
+
+export default Main
